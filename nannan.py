77211a326357badccec1916eaff8e12f3bc3a399
pcchen <pcchen@phys.nthu.edu.tw>
2015-06-16 20:43:00 +0800

    add syllable_2_alphanum

--- nannan.py
+++ nannan.py
@@ -1,4 +1,5 @@
 import re
+import unicodedata
 
 def syllable_structure(syllable):
   # lan2 -> l+a+n+2
@@ -49,3 +50,61 @@ def syllable_structure(syllable):
   else:
     print('no match')
     return False
+
+def syllable_2_alphanum(syllable):
+  # convert a syllable in unicode to alphanum representation
+  # oo & nn
+  oo_uni=u'\u0358'
+  nn_uni=u'ⁿ'
+  #
+  tone2_uni=u'\u0301'
+  tone3_uni=u'\u0300'
+  tone5_uni=u'\u0302'
+  tone7_uni=u'\u0304'
+  tone8_uni=u'\u030d'
+  #
+  tone6_uni=u'\u0301'
+  tone9_uni=u'\u0301'
+
+  #
+  syllable_alphanum=unicodedata.normalize('NFD',syllable)
+  # replace the unicode tones by number tones
+  # á -> 'a'+u'\u0301' ,etc
+  syllable_alphanum=syllable_alphanum.replace(tone2_uni,'2')
+  syllable_alphanum=syllable_alphanum.replace(tone3_uni,'3')
+  syllable_alphanum=syllable_alphanum.replace(tone5_uni,'5')
+  syllable_alphanum=syllable_alphanum.replace(tone7_uni,'7')
+  syllable_alphanum=syllable_alphanum.replace(tone8_uni,'8')
+  # POJ orthography
+  syllable_alphanum=syllable_alphanum.replace(oo_uni,'o')
+  syllable_alphanum=syllable_alphanum.replace(nn_uni,'nn')
+
+  # print('syllable_alphanum=',syllable_alphanum)
+  tone = ""
+  tone_re = re.compile("[0-9]")
+  # find all the number tones
+  m = tone_re.findall(syllable_alphanum)
+  if len(m) == 0:
+    # 1th or 4h tone
+    if syllable_structure(syllable_alphanum):
+      return syllable_alphanum
+    else:
+      print('invalid')
+  elif len(m) == 1:
+    # print m
+    syllable_alphanum=tone_re.sub('',syllable_alphanum)
+    # print('return', syllable_alphanum+m[0])
+    return syllable_alphanum+m[0]
+  else:
+    # not a valid syllable, return original syllable without any conversion
+    # print('return', syllable)
+    return syllable
+  # print m
+  # if m:
+  #   for each in m:
+  #     # tone = m.group(0)
+  #     # tone=
+  #     print each
+  #     # syllable_alphanum = tone_re.sub("", syllable_alphanum)
+  #     syllable_alphanum=syllable_alphanum + each
+  # return syllable_alphanum
